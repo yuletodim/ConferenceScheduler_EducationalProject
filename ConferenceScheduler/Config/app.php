207d9f1d90530612f2db6b0982b8f87ad765afec
Yulia <yuletodim@yahoo.es>
2015-11-22 16:07:33 +0200

    Bugs in Config class fixed.

---
+++ ConferenceScheduler/Config/app.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yulia
+ * Date: 22.11.2015
+ * Time: 14:15
+ */
+
+$config['namespaces']['Controllers'] = 'ConferenceScheduler/Controllers';
+$config['namespaces']['Models'] = 'ConferenceScheduler/Models';
+
+return $config;
